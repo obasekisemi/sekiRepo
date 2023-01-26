dbf61e331e1e25a1568dd781af749fb434baf97a
osemwengie obaseki <obasekisemi@gmail.com>
2023-01-26 18:55:25 +0100

    assignment

--- dayone.py
+++ dayone.py
@@ -1,6 +1,40 @@
+#mynum = 1
+#while mynum<=100:
+ #print (mynum)
+ #mynum =mynum+1
 
-print("hello")
-mylist= ["life","hard", "money", "life"]
-mylist.append("food")
-mylist.pop(0) 
-print (mylist)
+
+#for i in range(1, 11):
+   # num_int=str(i)
+   # print(num_int)
+
+
+
+#for booli in [True, False]:
+ #   booli_str= str(booli)
+  #  print(booli_str)
+
+
+#import random
+
+#for i in range(20):
+#   float_val= random.uniform(0.0,100.0)
+ #   float_str= str(float_val)
+  #  print (float_str)
+
+
+def age(yearofbirth):
+    return 2023-yearofbirth
+
+
+realage =age(int(input()))
+print (realage)
+if realage >=18:
+    print("you are old enough")
+else:
+    print("you are not old enough")
+
+
+
+
+
